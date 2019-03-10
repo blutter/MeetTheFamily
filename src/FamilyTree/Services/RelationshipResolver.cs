f08855a5b3cbcdc29d33dfddf6ed540caf0ac926
Benjamin Tse <benjamin.l.tse@gmail.com>
2019-03-10 17:40:38 +1100

    Add brother in law lookup

--- src/FamilyTree/Services/RelationshipResolver.cs
+++ src/FamilyTree/Services/RelationshipResolver.cs
@@ -17,6 +17,10 @@ namespace FamilyTree.Services
                     return person.Children.Where(child => child.IsFemale);
                 case Relationship.Siblings:
                     return person.Mother.Children.Where(mothersChild => !mothersChild.Equals(person));
+                case Relationship.BrotherInLaws:
+                    return person.Mother.Children
+                        .Where(mothersChild => (mothersChild.IsFemale && mothersChild.Spouse != null))
+                        .Select(sister => sister.Spouse);
                 default:
                     throw new NotImplementedException($"Unsupported relationship {relationship}");
                     break;
---
+++ src/FamilyTreeTests/Services/RelationshipResolverTests/BrothersInLawAreReturned.cs
@@ -0,0 +1,65 @@
+﻿using FamilyTree.Model;
+using FamilyTree.Services;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace FamilyTreeTests.Services.RelationshipResolverTests
+{
+    public class BrothersInLawAreReturned
+    {
+        private readonly Person _child;
+        private readonly List<Person> _brotherInLaws;
+        private IRelationshipResolver _relationshipResolver;
+
+        public BrothersInLawAreReturned()
+        {
+            _child = GivenAChildWithMarriedSiblings();
+            _brotherInLaws = WhenTheBrotherInLawsAreQueried();
+        }
+
+        private Person GivenAChildWithMarriedSiblings()
+        {
+            var parent = Person.Create("Eve", Gender.Female);
+            parent.SetSpouse(Person.Create("Adam", Gender.Male));
+
+            var marriedSister = Person.CreateChild(parent, "Alice", Gender.Female);
+            var brotherInLaw1 = Person.Create("Anthony", Gender.Male);
+            marriedSister.SetSpouse(brotherInLaw1);
+
+            var child = Person.CreateChild(parent, "Bob", Gender.Male);
+
+            var marriedBrother = Person.CreateChild(parent, "Christopher", Gender.Male);
+            var sisterInLaw = Person.Create("Carol", Gender.Female);
+            marriedBrother.SetSpouse(sisterInLaw);
+
+            var marriedSister2 = Person.CreateChild(parent, "Diane", Gender.Female);
+            var brotherInLaw2 = Person.Create("Doug", Gender.Male);
+            marriedSister2.SetSpouse(brotherInLaw2);
+
+            return child;
+        }
+
+        private List<Person> WhenTheBrotherInLawsAreQueried()
+        {
+            _relationshipResolver = new RelationshipResolver();
+            return _relationshipResolver.GetRelations(_child, Relationship.BrotherInLaws).ToList();
+        }
+
+        [Fact]
+        public void ThenAllTheBrotherInLawsAreReturned()
+        {
+            _brotherInLaws.Count.Should().Be(2);
+            _brotherInLaws.Should().OnlyContain(person => person.Gender == Gender.Male);
+        }
+
+        [Fact]
+        public void ThenTheBrotherInLawsAreReturnedInTheOrderTheirSpousesWereAdded()
+        {
+            _brotherInLaws.First().Should().Match<Person>(person => person.Name == "Anthony");
+            _brotherInLaws.Last().Should().Match<Person>(person => person.Name == "Doug");
+        }
+
+    }
+}
