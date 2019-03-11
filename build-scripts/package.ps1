d0af1ee81b4497566953dbb653af96f25fb64918
Ben Tse <benjamin.l.tse@gmail.com>
2019-03-11 23:34:53 +1100

    Remove any pre-existing package

--- build-scripts/package.ps1
+++ build-scripts/package.ps1
@@ -4,6 +4,8 @@ $PublishFile = "$PublishDirectory\MeetTheFamily.zip"
 
 git clean --force -d -x
 
+Remove-Item -LiteralPath $PublishDirectory -Force -Recurse -ErrorAction Ignore
+
 New-Item $PublishDirectory -ItemType Directory -Force
 
 Write-Host "Creating archive in $($PublishFile)"
