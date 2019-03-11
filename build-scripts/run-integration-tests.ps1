78fd2543b0cee666874864cbceeb89996ab283ea
Ben Tse <benjamin.l.tse@gmail.com>
2019-03-11 21:22:06 +1100

    Add integration tests

---
+++ build-scripts/run-integration-tests.ps1
@@ -0,0 +1,24 @@
+$RuntimeProject = "$PSScriptRoot\..\src\MeetTheFamily\MeetTheFamily.csproj"
+$TestSamplePath = "$PSScriptRoot\..\test\samples"
+$TestOutputPath = "$PSScriptRoot\..\test\test-output"
+
+Remove-Item -LiteralPath $TestOutputPath -Force -Recurse
+New-Item $TestOutputPath -ItemType Directory -Force
+
+$InputFiles = Get-ChildItem "$TestSamplePath\*.input.txt"
+
+foreach ($inputFile in $InputFiles) {
+    Write-Host "Running test on $($inputFile.FullName)"
+    $outputFilename = "$($TestOutputPath)/$((($inputFile.Name) -split "\.")[0]).output.txt"
+    $referenceOutputFilename = "$($TestSamplePath)/$((($inputFile.Name) -split "\.")[0]).output.txt"
+    Write-Host "Outputting to $($outputFilename)"
+    dotnet run --project .\src\MeetTheFamily\MeetTheFamily.csproj $inputFile.FullName | Out-File -Encoding ASCII $outputFilename
+
+    if (diff $(Get-Content $referenceOutputFilename) $(Get-Content $outputFilename)) {
+        Write-Error "Output for $($inputFile) is incorrect"
+    } else {
+        Write-Host "Output for $($inputFile) is correct"
+    }
+}
+
+
